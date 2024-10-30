41cf6e0dbad74bf349adcc0ada74e91d6539d4c4
McCarthy <mccarthymutebi10@gmail.com>
2024-10-30 15:27:20 +0300

    configure jest, setup test file, and chnages to typescript and eslint

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
 /** @type {import('jest').Config} */
+
+const path = require('path');
+
 const config = {
+  clearMocks: true,
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
+  setupFilesAfterEnv: [path.resolve(__dirname, 'test-tools', 'setup-tests.ts')],
+  testEnvironment: 'node',
+  testTimeout: 10000,
 };
 
 module.exports = config;
---
+++ test-tools/setup-tests.ts
@@ -0,0 +1,1 @@
+// Place here any code that needs to run before tests
